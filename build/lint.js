9bc3505a4c5fa7c0f869a2e9caa03d534ceac29c
Brent Lintner <brent.lintner@gmail.com>
2011-08-21 00:47:26 -0400

    added show nonerror option to project lint jake tast

--- build/lint.js
+++ build/lint.js
@@ -1,7 +1,7 @@
 module.exports = function (files) {
     var args = files && files.length > 0 ? files : ["."],
         spawn = require('child_process').spawn,
-        cmd = spawn("jshint", args),
+        cmd = spawn("jshint", args.concat(["--show-non-errors"])),
         sys = require('sys');
     cmd.stdout.on('data', sys.print);
     cmd.stderr.on('data', sys.print);
