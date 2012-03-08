f1d8b75dc2e8caa02b736dff84a21fe3fb04ccca
John Donahue <johnmdonahue@gmail.com>
2012-03-07 18:49:05 -0800

    update exec fix to use standard callback pattern

--- build/lint.js
+++ build/lint.js
@@ -1,13 +1,14 @@
 module.exports = function (files) {
     var args = files && files.length > 0 ? files : ["."],
         exec = require('child_process').exec,
-        opts = args.concat(["--config .jshintrc", "--show-non-errors"]).join(' '),
-        cmd = exec("jshint " + opts);
-
-    function write(data) {
-        process.stdout.write(new Buffer(data).toString("utf-8"));
-    }
-
-    cmd.stdout.on('data', write);
-    cmd.stderr.on('data', write);
+        opts = args.concat(["--config .jshintrc", "--show-non-errors"]).join(' ');
+    
+    exec("jshint " + opts, function (err, stdout, stderr) {
+        if (stdout) {
+            console.log(stdout);
+        }
+        if (stderr) {
+            console.log(stderr);
+        }
+    });
 };
