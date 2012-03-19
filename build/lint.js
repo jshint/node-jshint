64444db26015bb87293119d0398c6ccf2663e1b5
John Donahue <johnmdonahue@gmail.com>
2012-03-19 13:15:54 -0700

    use spawn with split --config option

--- build/lint.js
+++ build/lint.js
@@ -1,14 +1,13 @@
 module.exports = function (files) {
     var args = files && files.length > 0 ? files : ["."],
-        exec = require('child_process').exec,
-        opts = args.concat(["--config .jshintrc", "--show-non-errors"]).join(' ');
-    
-    exec("jshint " + opts, function (err, stdout, stderr) {
-        if (stdout) {
-            console.log(stdout);
-        }
-        if (stderr) {
-            console.log(stderr);
-        }
-    });
+        spawn = require('child_process').spawn,
+        cmd = spawn("jshint", args.concat(["--config", ".jshintrc", "--show-non-errors"]));
+
+    function write(data) {
+        process.stdout.write(new Buffer(data).toString("utf-8"));
+    }
+
+    cmd.stdout.on('data', write);
+    cmd.stderr.on('data', write);
+
 };
